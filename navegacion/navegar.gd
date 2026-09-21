421274599f212f2bc17526111cc0e6cf49de70c1
Diego Abanto <dabantoibarguen@colgate.edu>
2026-09-20 19:51:52 -0500

    Hex Draft created

---
+++ navegacion/navegar.gd
@@ -0,0 +1,15 @@
+extends TileMapLayer
+
+
+var source_id = 4
+
+func _ready() -> void:
+	pass
+	
+func _input(ev: InputEvent) -> void:
+	if ev is InputEventMouse and ev.is_pressed():
+		if ev.button_index == 1:
+			var coords = self.local_to_map(get_global_mouse_position())
+			print(coords)
+			
+			
