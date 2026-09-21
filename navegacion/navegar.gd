a6855cce45ce7d43e5782295fa71c89e560b60d0
Diego Abanto <dabantoibarguen@colgate.edu>
2026-09-20 22:06:11 -0500

    Evitar movimento fuera del mapa

--- navegacion/navegar.gd
+++ navegacion/navegar.gd
@@ -6,9 +6,15 @@ func _ready() -> void:
 	pass
 	
 func _input(ev: InputEvent) -> void:
-	if ev is InputEventMouse and ev.is_pressed():
-		if ev.button_index == 1:
+	if ev is InputEventMouse:
+		if ev.is_pressed() and ev.button_index == 1:
 			var coords = self.local_to_map(get_global_mouse_position())
-			%barco.global_position = map_to_local(coords) + Vector2(0, 8)
+			if get_cell_atlas_coords(coords) != Vector2i(-1, -1):
+				%barco.global_position = map_to_local(coords) + Vector2(0, 7)
+		else:
+			pass
+			
+func _physics_process(delta: float) -> void:
+	pass
 			
 			
