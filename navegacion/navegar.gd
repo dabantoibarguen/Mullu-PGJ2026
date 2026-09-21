7a52e5c82ae98e87ecca7204fb1a12d75f7d2e89
Diego Abanto <dabantoibarguen@colgate.edu>
2026-09-20 20:36:19 -0500

    Moving boat

--- navegacion/navegar.gd
+++ navegacion/navegar.gd
@@ -1,6 +1,5 @@
 extends TileMapLayer
 
-
 var source_id = 4
 
 func _ready() -> void:
@@ -10,6 +9,6 @@ func _input(ev: InputEvent) -> void:
 	if ev is InputEventMouse and ev.is_pressed():
 		if ev.button_index == 1:
 			var coords = self.local_to_map(get_global_mouse_position())
-			print(coords)
+			%barco.global_position = map_to_local(coords) + Vector2(0, 8)
 			
 			
