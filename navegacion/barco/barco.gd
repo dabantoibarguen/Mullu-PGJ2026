da62a4c4b5289f596e885301743a98c7dc267e21
Sergio Moy <sergio.moy@gmail.com>
2026-09-24 23:52:38 -0500

    boat sprite now flips horizontally based on direction

--- navegacion/barco/barco.gd
+++ navegacion/barco/barco.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @onready var oceanMap = %OceanMap
 @onready var label = $Label
 @onready var energyText = %TotalEnergy
+@onready var sprite = $Sprite2D
 
 var speed = 75
 var cur_coords
@@ -17,7 +18,14 @@ func _ready():
 	energyText.text = "30/30"
 
 func _physics_process(_delta):
-	velocity = global_position.direction_to(target).normalized() * speed
+	var direction = global_position.direction_to(target)
+	velocity = direction.normalized() * speed
+	if(direction[0]>=0 and direction[1]>=0):
+		sprite.flip_h = true
+	elif(direction[0]<0 and direction[1]<0):
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
 	if global_position.distance_to(target)<1:
 		velocity = Vector2(0,0)
 	else:
